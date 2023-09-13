391551cce95bf7b952a17f2cc43f82592a559099
Harley Oliveira <harleyoliveiramg@gmail.com>
2023-09-13 17:33:14 -0300

    Exercicio1 commit

---
+++ 1/atividade.java
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-witdh, initial-scale=1.0">
+    <title>Hello, Word</title>
+</head>
+<body>
+    <div id="olamundo"></div>
+    <script type="text/javascript">
+    document.getElementById('olamundo').innerHTML = 'Hello, Worl!';
+    </script>
+</body>
+</ht
